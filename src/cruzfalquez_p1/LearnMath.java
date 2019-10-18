6039ca8529d925e860ee0139d5f2202ac81b1621
Vanillacruz <54645562+Vanillacruz@users.noreply.github.com>
2019-10-18 17:00:59 -0400

    found my .class

--- src/cruzfalquez_p1/LearnMath.java
+++ src/cruzfalquez_p1/LearnMath.java
@@ -1,10 +1,63 @@
 /*
  	Daniela Cruz Falquez
-	Programming Assignment 2- Problem 3
+	Programming Assignment 3- Problem 1
 	10/16/2019
 */
-
 package cruzfalquez_p1;
 
+import java.security.SecureRandom;
+import java.util.Scanner;
+
 public class LearnMath {
+    public static void main (String[] args) {
+        int answer = 0;
+        boolean correct;
+        boolean quit = false;
+        SecureRandom random = new SecureRandom();
+        Scanner scnr = new Scanner(System.in);
+
+        while(!quit){
+            answer = generateQuestions(random);
+            correct = generateResponse(scnr.nextInt(), answer);
+
+            while(!correct){
+                correct = generateResponse(scnr.nextInt(), answer);
+            }
+        }
+
+
+    }
+
+    public static int generateQuestions(SecureRandom rand){
+        int int1 = rand.nextInt(10);
+        int int2 = rand.nextInt(10);
+
+        System.out.println("How much is " + int1 + " times " + int2 + "?");
+
+        return int1*int2;
+    }
+
+    public static boolean generateResponse(int response, int correctAnswer){
+        if(response == correctAnswer){
+            System.out.println("Very good!");
+            return true;
+        }
+        else{
+            System.out.println("No please try again.>again.");
+            return false;
+        }
+
+    }
+
+    public void determineDifficultyLevel(){
+
+    }
+
+    public void determineProblemType(){
+
+    }
+
+
+
+
 }
--- src/cruzfalquez_p2/SavingsAccount.java
+++ src/cruzfalquez_p2/SavingsAccount.java
@@ -1,12 +1,12 @@
 /*
  	Daniela Cruz Falquez
-	Programming Assignment 2- Problem 3
+	Programming Assignment 3- Problem 2
 	10/15/2019
 */
 
 package cruzfalquez_p2;
 
 public class SavingsAccount {
-    static private annualInterestRate;
-    private savingsBalance;
+    //static private annualInterestRate;
+    //private savingsBalance;
 }
