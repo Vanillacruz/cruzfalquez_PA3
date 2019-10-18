a536d343d49a9164e4ad11ae7acef1fe95c0c617
Vanillacruz <54645562+Vanillacruz@users.noreply.github.com>
2019-10-18 15:50:33 -0400

    uploadind packges to git

---
+++ src/cruzfalquez_p1/LearnMath.java
@@ -0,0 +1,10 @@
+/*
+ 	Daniela Cruz Falquez
+	Programming Assignment 2- Problem 3
+	10/16/2019
+*/
+
+package cruzfalquez_p1;
+
+public class LearnMath {
+}
---
+++ src/cruzfalquez_p2/SavingsAccount.java
@@ -0,0 +1,12 @@
+/*
+ 	Daniela Cruz Falquez
+	Programming Assignment 2- Problem 3
+	10/15/2019
+*/
+
+package cruzfalquez_p2;
+
+public class SavingsAccount {
+    static private annualInterestRate;
+    private savingsBalance;
+}
