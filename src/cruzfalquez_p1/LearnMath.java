3d7f12126a96151990acecaef4001c683021f273
Vanillacruz <54645562+Vanillacruz@users.noreply.github.com>
2019-10-18 21:28:01 -0400

    done with part 3

--- src/cruzfalquez_p1/LearnMath.java
+++ src/cruzfalquez_p1/LearnMath.java
@@ -10,22 +10,73 @@ import java.util.Scanner;
 
 public class LearnMath {
     public static void main (String[] args) {
+        final int NUM_QUESTIONS = 10;
         int answer = 0;
+        int choice = 0;
+        int response = 0;
+        String ans = "";
+        double percentage;
         boolean correct;
         boolean quit = false;
+        int numCorrect;
         SecureRandom random = new SecureRandom();
         Scanner scnr = new Scanner(System.in);
 
         while(!quit){
-            answer = generateQuestions(random);
-            correct = generateResponse(scnr.nextInt(), answer);
+            numCorrect = 0;
+            for(int i = 0; i <NUM_QUESTIONS; i++){ //CHANGE TO 10
+                answer = generateQuestions(random);
+                response = scnr.nextInt();
+                correct = generateResponse(response, answer, random);
 
-            while(!correct){
-                correct = generateResponse(scnr.nextInt(), answer);
+                if(correct){
+                    numCorrect++;
+                }
+
+                /*while(!correct){
+                    response = scnr.nextInt();
+                    correct = generateResponse(response, answer, random);
+                }*/
             }
+
+            System.out.println("\nNumber of correct responses: " + numCorrect);
+            System.out.println("Number of incorrect responses " + (NUM_QUESTIONS-numCorrect));
+
+            percentage = ((1.0*numCorrect)/NUM_QUESTIONS)*100; // stop int division
+            if(percentage<75){
+                System.out.println( "\nPlease ask your teacher for extra help.");
+            }else if(percentage >= 75){
+                System.out.println("\nCongratulations, you are ready to go to the next level!");
+            }
+
+            System.out.println("\nThank you for using this program!\nWould you like to begin a new session? (y/n)");
+            ans = scnr.next();
+
+            choice = 0;
+            if (ans.equalsIgnoreCase("y") || ans.equalsIgnoreCase("yes")) {
+                choice = 1;
+            } else if (ans.equalsIgnoreCase("n") || ans.equalsIgnoreCase("no")) {
+                quit = true;
+                choice = 2;
+            }
+            //ensures a valid response
+            while ((choice != 1) && (choice != 2)) {
+                System.out.println("Sorry, that was not a valid response. \nPlease enter again:");
+                System.out.println("Enter y/Y/yes/YES if another person would like to rate these topics.");
+                System.out.println("Enter n/N/no/NO if all responses have been logged.");
+                ans = scnr.next();
+                System.out.print("\n");
+                if (ans.equalsIgnoreCase("y") || ans.equalsIgnoreCase("yes")) {
+                    choice = 1;
+                } else if (ans.equalsIgnoreCase("n") || ans.equalsIgnoreCase("no")) {
+                    quit = true;
+                    choice = 2;
+                }
+            }
+
         }
 
-
+        scnr.close();
     }
 
     public static int generateQuestions(SecureRandom rand){
@@ -37,13 +88,47 @@ public class LearnMath {
         return int1*int2;
     }
 
-    public static boolean generateResponse(int response, int correctAnswer){
+    public static boolean generateResponse(int response, int correctAnswer, SecureRandom rand){
+        int randomNum = rand.nextInt((1)+4);
+
         if(response == correctAnswer){
-            System.out.println("Very good!");
+            switch(randomNum) {
+                case 1:
+                    System.out.println("Very good!");
+                    break;
+                case 2:
+                    System.out.println("Excellent!");
+                    break;
+                case 3:
+                    System.out.println("Nice work!");
+                    break;
+                case 4:
+                    System.out.println("Keep up the good work!");
+                    break;
+                default:
+                    System.out.println("Very good!");
+                    break;
+            }
             return true;
         }
         else{
-            System.out.println("No please try again.>again.");
+            switch(randomNum) {
+                case 1:
+                    System.out.println("No please try again.");
+                    break;
+                case 2:
+                    System.out.println("Wrong. Try once more.");
+                    break;
+                case 3:
+                    System.out.println("Don't give up!");
+                    break;
+                case 4:
+                    System.out.println("No. Keep trying.");
+                    break;
+                default:
+                    System.out.println("No please try again.");
+                    break;
+            }
             return false;
         }
 
